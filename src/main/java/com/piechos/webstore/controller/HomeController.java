bc76dfffc7c0913828ee8b05c67821c8ced1d3d6
Mateusz Piechowiak <mateusz.s.piechowiak@gmail.com>
2016-10-30 13:25:40 +0100

    Initial commit

---
+++ src/main/java/com/piechos/webstore/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.piechos.webstore.controller;
+
+/**
+ * Created by Mati on 24.10.2016.
+ */
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String welcome(Model model) {
+        model.addAttribute("greeting", "Witaj w sklepie internetowym!");
+        model.addAttribute("tagline", "Wyjątkowym i jedynym sklepie internetowym");
+
+        return "welcome";
+    }
+}
