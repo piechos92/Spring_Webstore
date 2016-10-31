4aaa697b80f67a87ed6413a992a89d73937d3363
Mateusz Piechowiak <mateusz.s.piechowiak@gmail.com>
2016-10-31 10:53:39 +0100

    Add relative mapping for ProductController and default mapping method for class

--- src/main/java/com/piechos/webstore/controller/ProductController.java
+++ src/main/java/com/piechos/webstore/controller/ProductController.java
@@ -7,13 +7,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/products")
 public class ProductController {
 
     @Autowired
     private ProductService productService;
 
-    @RequestMapping("/products")
+    @RequestMapping()
     public String list(Model model) {
+        model.addAttribute("products", productService.getAllProducts());
+        return "products";
+    }
+
+    @RequestMapping("/all")
+    public String allProducts(Model model) {
         model.addAttribute("products", productService.getAllProducts());
         return "products";
     }
